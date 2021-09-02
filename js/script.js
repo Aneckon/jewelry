a8bedb2062b58aaa1f90766437f834c7185c826c
Aneckon <83111616+Aneckon@users.noreply.github.com>
2021-09-02 17:21:54 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,42 +1,81 @@
-$(window).scroll(function(event){
+$(window).scroll(function (event) {
   var scroll = $(window).scrollTop();
 
   if (scroll >= 1) {
-    $(".top").addClass("show");
+    $(".top").addClass("shows");
     $(".header__scroll").addClass("header__block");
-    $(".none").addClass("show");
+    $(".none").addClass("shows");
   } else {
-    $(".top").removeClass("show");
+    $(".top").removeClass("shows");
     $(".header__scroll").removeClass("header__block");
-    $(".none").removeClass("show");
+    $(".none").removeClass("shows");
   }
 });
 
-$('a[href="#top"]').on('click',function(){
-    $('html, body').animate({scrollTop: 0}, 'slow');
-    return false;
+$('a[href="#top"]').on('click', function () {
+  $('html, body').animate({ scrollTop: 0 }, 'slow');
+  return false;
 });
 
 
-var swiper = new Swiper(".mySwiper", {
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
-
-var swiper2 = new Swiper(".mySwiper", {
-  pagination: {
-    el: ".swiper-pagination2",
-    clickable: true,
-  },
-});
+// var swiper = new Swiper(".mySwiper", {
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
 
 
-$(document).ready(function() {
-  $('.header__burger-img').click(function() {
-      $('.header__mobile-menu').toggleClass('open__menu');
-      $('.header__burger-img').toggleClass('header__burger-img__remove');
-      $('body').toggleClass('fixed');
+$(document).ready(function () {
+  $('.header__burger-img').click(function () {
+    $('.header__mobile-menu').toggleClass('open__menu');
+    $('.header__burger-img').toggleClass('header__burger-img__remove');
+    $('body').toggleClass('fixed');
   });
+});
+
+
+
+var $status = $('.customslider .pagingInfo span.cp1');
+var $status1 = $('.customslider .pagingInfo  span.cp2');
+var $slickElement = $('.customslider .slider-centermode');
+
+$slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+  if (slick.slideCount < 10) {
+    var i = (currentSlide ? currentSlide : 0) + 1;
+    $status.text('0' + i);
+  } else {
+    var i = (currentSlide ? currentSlide : 0) + 1;
+    $status.text(i);
+  }
+
+  if (slick.slideCount < 10) {
+    var i = (currentSlide ? currentSlide : 0) + 1;
+    $status1.text(' 0' + slick.slideCount);
+  } else {
+    var i = (currentSlide ? currentSlide : 0) + 1;
+    $status1.text(slick.slideCount);
+  }
+
+});
+
+$slickElement.slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true,
+  arrows: true,
+  prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+  nextArrow: '<button type="button" class="slick-next">Next</button>',
+  speed: 1000,
+  centerPadding: '31.1%',
+  responsive: [{
+    breakpoint: 768,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      centerPadding: 0,
+    }
+  }]
 });
