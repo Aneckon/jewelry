1f19ab80eab9704705ec9d76dfa194dee98b45a0
Aneckon <83111616+Aneckon@users.noreply.github.com>
2021-09-02 17:29:53 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -19,10 +19,10 @@ $('a[href="#top"]').on('click', function () {
 
 
 var swiper = new Swiper(".mySwiper", {
-  pagination: {
-    el: ".swiper-pagination",
+   pagination: {
+     el: ".swiper-pagination",
      clickable: true,
- },
+   },
 });
 
 
@@ -32,50 +32,4 @@ $(document).ready(function () {
     $('.header__burger-img').toggleClass('header__burger-img__remove');
     $('body').toggleClass('fixed');
   });
-});
-
-
-
-var $status = $('.customslider .pagingInfo span.cp1');
-var $status1 = $('.customslider .pagingInfo  span.cp2');
-var $slickElement = $('.customslider .slider-centermode');
-
-$slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-  if (slick.slideCount < 10) {
-    var i = (currentSlide ? currentSlide : 0) + 1;
-    $status.text('0' + i);
-  } else {
-    var i = (currentSlide ? currentSlide : 0) + 1;
-    $status.text(i);
-  }
-
-  if (slick.slideCount < 10) {
-    var i = (currentSlide ? currentSlide : 0) + 1;
-    $status1.text(' 0' + slick.slideCount);
-  } else {
-    var i = (currentSlide ? currentSlide : 0) + 1;
-    $status1.text(slick.slideCount);
-  }
-
-});
-
-$slickElement.slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  dots: true,
-  centerMode: true,
-  focusOnSelect: true,
-  arrows: true,
-  prevArrow: '<button type="button" class="slick-prev">Previous</button>',
-  nextArrow: '<button type="button" class="slick-next">Next</button>',
-  speed: 1000,
-  centerPadding: '31.1%',
-  responsive: [{
-    breakpoint: 768,
-    settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      centerPadding: 0,
-    }
-  }]
 });
